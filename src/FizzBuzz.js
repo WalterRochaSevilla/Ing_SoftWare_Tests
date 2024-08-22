f97bef2bc18dcf4b86e45bf5fd5084d4443a9046
Walter Rocha <walterrochasevilla@gmail.com>
2024-08-22 10:11:59 -0400

    Generar Secuencia 1->n siguiendo las reglas implementadas

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,8 +1,18 @@
-function generarFizzBuzz(n){
-    let resp ="";
-    if (n % 3 == 0) resp += "Fizz";
-    if (n % 5 == 0) resp += "Buzz";
-    if (n % 3 != 0 && n % 5 != 0)resp +=n;
+function generarFizzBuzz(n) {
+    let resp = "";
+    if (n % 3 === 0) resp += "Fizz";
+    if (n % 5 === 0) resp += "Buzz";
+    if (n % 3 !== 0 && n % 5 !== 0) resp += n;
     return resp;
 }
-export default generarFizzBuzz;
+
+function generarSecuenciaFizzBuzz(n) {
+    let resp = "";
+    for (var i = 1; i <= n; i += 1) {
+        resp += generarFizzBuzz(i);
+        if (i < n) resp += ", ";
+    }
+    return resp;
+}
+
+export { generarFizzBuzz, generarSecuenciaFizzBuzz };
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,4 +1,4 @@
-import generarFizzBuzz from "./FizzBuzz"
+import { generarFizzBuzz, generarSecuenciaFizzBuzz } from "./FizzBuzz";
 
 describe("FizzBuzz", () => {
   it("Genera el mismo número si no sigue regla", () => {
@@ -18,5 +18,8 @@ describe("FizzBuzz", () => {
   });
   it("Si el número es múltiplo de 3 y 5 se retornará FizzBuzz", () => {
     expect(generarFizzBuzz(15)).toEqual("FizzBuzz");
+  });
+  it("Devolverá una secuencia 1-n siguiendo las reglas implementadas antes", () => {
+    expect(generarSecuenciaFizzBuzz(15)).toEqual("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz");
   });
 });
